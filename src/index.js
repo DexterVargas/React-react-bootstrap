6b3d2053349c29a8c26b66e48a6ffe4e48485d18
dextervargasDev <dextervargas0912@gmail.com>
2023-03-31 06:30:15 +0800

    4. assignment. react-bootstrap

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import * as React from 'react';
+import Alert from 'react-bootstrap/Alert';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+function Example() {
+  return (
+    <Alert dismissible variant="danger">
+      <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+      <p>
+        Change this and that and try again. asda sdasd 
+      </p>
+    </Alert>
+  )
+}
+
+
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Example />
   </React.StrictMode>
 );
 
